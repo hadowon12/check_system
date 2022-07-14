a2ca179cc3dca926c6fff63c9dc4b1deaeed878d
c_falcon12 <hadowon12@naver.com>
2022-07-14 15:43:19 +0900

    sanghoon edit

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include <windows.h>
 #include <time.h>
 
-
-
 int check() //프로그램 구동 함수
 {
     int start;
@@ -16,48 +14,43 @@ int check() //프로그램 구동 함수
     printf("4.종료\n");
     printf("-----------------------------------\n");
     printf("원하는 항목의 숫자를 입력하고 엔터(Enter) : ");
-    scanf("%d",&start);
-    if(start == 4)
-    {
+    scanf("%d", &start);
+    if (start == 4) {
         exit(0);
-    }
-    else
-    {
+    } else {
         system("CLS");
         return start;
     }
 }
 
-int main()
-{
-    int date[1000][2] = {{0,},}; //출석 일수 0번째 줄은 고유번호 존재 유무 1번째줄은 출석 일수
+int main() {
+//    int date[1000][2] = {{0,},}; //출석 일수 0번째 줄은 고유번호 존재 유무 1번째줄은 출석 일수
+    int date[1000][2] = {0};
     int sys; // 어떤 동작을 할껀지 지정하는 함수
-    int i,j,k;
-    while(1) {
+    int i, j, k, a;
+    while (1) {
         sys = check();
-        if (sys == 1) {
-            printf("고유번호를 입력하세요:");
-            scanf("%d", &i);
-            date[i][1] += 1; //출석일수 증가
-        } else if (sys == 2) {
-            srand(time(NULL));
-            for (;;) {
-                j = rand() % 1000;
-                if (date[j][0] == 1) {
-                    date[j][0] += 1;// 고유번호 더이상 안나오게 하는것
-                    break;
+        switch (sys) {
+            case 1:
+                printf("고유번호를 입력하세요:");
+                scanf("%d", &i);
+                date[i][1] += 1; //출석일수 증가
+                break;
+            case 2:
+                srand(time(NULL));
+                j = rand() % 1000; //j의 범위 0~999
+                if (date[j][0] == 0) {
+                    printf("당신의 고유번호는 %d 입니다.", j);
+                    date[j][0] += 1;
                 }
-            }
-            printf("당신의 고유번호는 %d 입니다.", j);
-        }
-            else if(sys == 3){
-            for(int a=0;a<1000;a++)
-            {
-                if(date[a][0] == 1)
-                {
-                    printf("고유번호 %d 출석일수는 %d 입니다.\n",a,date[a][1]);
+                break;
+            case 3:
+                for (a = 0; a < 1000; a++) {
+                    if (date[a][0] != 0) {
+                        printf("고유번호 %d 출석일수는 %d 입니다.\n", a, date[a][1]);
+                    }
                 }
-            }
+                break;
         }
     }
 }
